28c73f88e17bea55d342eae6fbe17b5899ecd936
Radoslaw3 <37849836+Radoslaw3@users.noreply.github.com>
2019-12-22 04:55:35 +0100

    Update Main.java
    Add 122

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -14,3 +14,21 @@ public class Main {
 // Sklep
 
 // passleader
+122.
+import java.unit.*;
+????
+public class Ref
+{
+public static void main(String[] args)
+{
+StringBuilder s1 = new StringBuilder("Hello Java!");
+String s2 = s1.toString();
+List<String> lst = new ArrayList<String>();
+lst.add(s2);
+System.out.println(s1.getClass());
+System.out.println(s2.getClass());
+System.out.println(lst.getClass());
+}
+}
+
+
