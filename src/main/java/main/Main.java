1d6250989a5a305d5a4844fe86f114c4a3d75816
Radoslaw3 <37849836+Radoslaw3@users.noreply.github.com>
2019-12-24 01:08:58 +0100

    Update Main.java
    126

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -58,6 +58,20 @@ System.out.println(28+5<=4+29);
 System.out.println((28+5) <= (4+29));
 
 126.
+public class Equal{
+psvm
+String str1 = "Java";
+String[] str2 = {"J","a","v","a"};
+String str3 = "";
+for(String str : str2){
+str3 = str3+str;
+}
+boolean b1 = (str1 == str3);
+boolean b2 = (str1.equals(str3));
+System.out.print(b1+", "+b2);
+}
+
+
 
 
 
